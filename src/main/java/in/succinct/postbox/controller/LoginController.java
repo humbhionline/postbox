16f254c1aaed3c8703010853fddc87ec9d874ac4
Venky <venkatramanm@gmail.com>
2025-06-29 12:24:13 +0530

    local Login also supported

--- src/main/java/in/succinct/postbox/controller/LoginController.java
+++ src/main/java/in/succinct/postbox/controller/LoginController.java
@@ -1,8 +1,10 @@
 package in.succinct.postbox.controller;
 
+import com.venky.core.util.ObjectUtil;
 import com.venky.swf.controller.Controller;
 import com.venky.swf.controller.annotations.RequireLogin;
 import com.venky.swf.path.Path;
+import com.venky.swf.routing.Config;
 import com.venky.swf.views.RedirectorView;
 import com.venky.swf.views.View;
 
@@ -14,6 +16,14 @@ public class LoginController extends Controller {
     @Override
     @RequireLogin(false)
     public View index() {
-        return new RedirectorView(getPath(),"/oid","login?SELECTED_OPEN_ID=HUMBOL");
+        if (ObjectUtil.equals("GET",getPath().getRequest().getMethod())) {
+            if (Config.instance().getOpenIdProviders().isEmpty()){
+                return super.login();
+            }else {
+                return new RedirectorView(getPath(),"/oid","login?SELECTED_OPEN_ID=HUMBOL");
+            }
+        }else {
+            return super.login();
+        }
     }
 }
